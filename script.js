285d77b70c163212afe5416a279a8e3dbcc9adb3
Rohit Dahale <151941395+dahalerohit@users.noreply.github.com>
2024-10-05 11:17:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let charfreqcount = {}
+	for(let char of str)
+	{
+       if(charfreqcount[char])
+	   {
+		   charfreqcount[char]++
+	   }
+		else {
+		   charfreqcount[char] = 1;
+		}
+	}
+	for(char of str){
+		if(charfreqcount[char]===1)
+		{
+			return char;
+		}
+		
+	}
+	return null;
 }
-const input = prompt("Enter a string");
-alert(firstNonRepeatedChar(input)); 
+//const input = prompt("Enter a string");
+//alert(firstNonRepeatedChar(input)); 
